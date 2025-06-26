67acc90413dc32b274ae7a132847f77cdbef0013
Prajwal L K Urs <153601054+iprajwallkurs@users.noreply.github.com>
2025-06-26 22:58:00 +0530

    Modified CounterModel

--- app/src/main/java/com/example/countermvvm/CounterModel.kt
+++ app/src/main/java/com/example/countermvvm/CounterModel.kt
@@ -2,7 +2,7 @@ package com.example.countermvvm
 
 data class CounterModel (var count : Int)
 
-class CounterRepository{
+class CounterRepository{//repository is a desine pattern which acts as an API or commonly called as a moderator
     private var _counter = CounterModel(0)
 
     fun getCounter() = _counter
